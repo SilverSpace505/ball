a339f7633496dc8a0f2d3057ae09dd7756b99f3c
Overscore <Overscore34@users.noreply.github.com>
2025-12-16 16:16:34 +1030

    asdwasdw

--- player.gd
+++ player.gd
@@ -1,9 +1,10 @@
 extends CharacterBody3D
 
-var speed = 1
-var maxSpeed = 5
+var speed = 5
+var minSpeed = 15
+var maxSpeed = 50
 var jumpHeight = 100
-var turnSpeed = 20
+var turnSpeed = 0.02
 
 
 # Called when the node enters the scene tree for the first time.
@@ -19,31 +20,31 @@ func _process(delta: float) -> void:
 	else:
 		gravity = 0
 	
-	var turnDir = Input.get_axis("left", "right")
+	var turnDir = Input.get_axis("right", "left")
 	if turnDir:
-		$".".transform.rotation.y += turnDir * turnSpeed
+		$".".rotate(Vector3(0, 1, 0).normalized(), turnDir * turnSpeed)
 		
-	var direction := Input.get_axis("slow", "go")
+	var direction := Input.get_axis("slow", "go") * basis.z
 	#var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		#velocity.x += direction.x * speed
 		#velocity.z += direction.z * speed
-		velocity.x = lerpf(velocity.x, direction * speed, 0.2)
-		velocity.z = lerpf(velocity.z, direction * speed, 0.02)
+		velocity.x = lerpf(velocity.x, -direction.x * speed, 0.2)
+		velocity.z = lerpf(velocity.z, -direction.z * speed, 0.2)
 	else:
 		velocity.x = move_toward(velocity.x, 0, speed)
 		velocity.z = move_toward(velocity.z, 0, speed)
 	if Input.is_action_pressed("go"):
 		speed = lerpf(speed, maxSpeed, 0.002 * speed)
 	elif Input.is_action_pressed("slow"):
-		speed = lerpf(speed, 1, 0.02 / speed)
+		speed = lerpf(speed, minSpeed, 0.02 / speed)
 	else:
-		speed = lerpf(speed, 1, 0.02)
+		speed = lerpf(speed, minSpeed, 0.02)
 	print_debug(speed)
 	print_debug(velocity)
 	
 	if Input.is_action_just_pressed("jump"):
 		velocity.y += jumpHeight
 	
-	velocity *= 0.8
+	#velocity *= 0.8
 	move_and_slide()
