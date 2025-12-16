52053f57831a0abe250921965f16178992e16e62
Overscore <Overscore34@users.noreply.github.com>
2025-12-16 20:05:11 +1030

    kill me now

--- player.gd
+++ player.gd
@@ -1,5 +1,6 @@
 extends CharacterBody3D
 
+<<<<<<< HEAD
 var speed = 5
 var minSpeed = 15
 var maxSpeed = 50
@@ -7,6 +8,15 @@ var jumpHeight = 100
 var turnSpeed = 0.02
 
 
+=======
+@export var speed = 3
+@export var minSpeed = 15
+@export var maxSpeed = 50
+@export var jumpHeight = 100
+@export var turnSpeed = 0.02
+@export var friction = 0.1
+
+>>>>>>> parent of 748e8b7 (AAAAAAAAAAHHHHHHHH)
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -16,7 +26,11 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	var gravity = get_gravity()
 	if !is_on_floor():
+<<<<<<< HEAD
 		velocity = gravity
+=======
+		velocity += gravity
+>>>>>>> parent of 748e8b7 (AAAAAAAAAAHHHHHHHH)
 	else:
 		gravity = 0
 	
@@ -32,8 +46,13 @@ func _process(delta: float) -> void:
 		velocity.x = lerpf(velocity.x, -direction.x * speed, 0.2)
 		velocity.z = lerpf(velocity.z, -direction.z * speed, 0.2)
 	else:
+<<<<<<< HEAD
 		velocity.x = move_toward(velocity.x, 0, speed)
 		velocity.z = move_toward(velocity.z, 0, speed)
+=======
+		velocity.x = move_toward(velocity.x, 0, friction)
+		velocity.z = move_toward(velocity.z, 0, friction)
+>>>>>>> parent of 748e8b7 (AAAAAAAAAAHHHHHHHH)
 	if Input.is_action_pressed("go"):
 		speed = lerpf(speed, maxSpeed, 0.002 * speed)
 	elif Input.is_action_pressed("slow"):
