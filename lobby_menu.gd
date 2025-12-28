511322f1467fb06c6c14b8084494e05a02d7fdd5
Overscore <Overscore34@users.noreply.github.com>
2025-12-28 20:33:18 +1030

    lobbby

---
+++ lobby_menu.gd
@@ -0,0 +1,16 @@
+extends Control
+
+var players = []
+var mapLength = 100
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _addPlayer():
+	players.insert(0,Global.username)
+	
