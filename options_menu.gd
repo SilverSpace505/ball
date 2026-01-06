c367f0ea7cf4268ddbe87441fa2b99f3e0856eb5
bemannU <140582743+bemannU@users.noreply.github.com>
2026-01-06 16:16:19 +1030

    .

--- options_menu.gd
+++ options_menu.gd
@@ -12,9 +12,12 @@ func _process(delta: float) -> void:
 
 
 func _on_back_pressed() -> void:
+	Sfx.get_node("clickSFX").play()
 	$AnimationPlayer.play_backwards("optionsPressed")
 	await get_tree().create_timer(0.6).timeout
 	get_tree().change_scene_to_file("res://main_menu.tscn")
+	
+	
 
 
 func _on_vol_slider_value_changed(value: float) -> void:
